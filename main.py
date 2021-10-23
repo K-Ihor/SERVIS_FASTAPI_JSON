f8729af7842c19f66514cb16a15293a2d209be21
K-Ihor <i.kucherov.m@gmail.com>
2021-10-23 12:05:08 +0200

    versiom0.1

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI() # экземпрляр app
 
-
-
-# @app.get("/")
-# def index_page(): 
-#     with open('page.html', 'r') as f:
-#         page = f.read()
-#         return Response(page, media_type="text/html")
-
 def standardization_phone_number(num1: str) -> str:
     result = re.findall(r'\d', num1)
     if 10 <= len(result) <= 11:
@@ -20,7 +12,7 @@ def standardization_phone_number(num1: str) -> str:
             if result[0] == "9": # and result[1] == "1" and result[2] == "2":
                 return f"8 ({''.join(result[0:3])}) {''.join(result[3:6])}-{''.join(result[6:8])}-{''.join(result[8:10])}"
             else:
-                return''.join(result)
+                return ''.join(result)
         elif len(result) == 11: 
             if result[0] == "7" or result[0] == "8":
                 result[0] = "8 "
@@ -33,30 +25,10 @@ def standardization_phone_number(num1: str) -> str:
         return ''.join(result)
 
 
-
-
-
-
-# def standardization_phone_number(num1: str) -> str:
-#     num = "".join(filter(str.isdigit, num1))
-#     if len(num) < 10 or len(num) > 11 or num[-10] != '9':
-#         print(num)
-#     phone = "8 (9{}{}) {}{}{}-{}{}-{}{}".format(*num[-9:])
-#     # print(phone)
-#     return phone
-
-
 @app.post("/unify_phone_from_json")
 def process_page(data: dict = Body(...)):
     input_phone = data["userphone"]
     response = standardization_phone_number(input_phone)      
-    # response = Response(
-    #     json.dumps({
-    #         "success": True,
-    #         "message": f"Привет! Ваш номер<br />{input_phone}"
-    #     }),
-    #      media_type='application/json')
-    #return response
     return HTMLResponse(content=response, status_code=200)
 
 
